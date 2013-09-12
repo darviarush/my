dfe484eed096c45521d6117cd56501b8edf23dc2
U-REA\Лели <@REA.(none)>
2013-09-12 23:45:15 +0400

    save

--- imperial/imperial.c
+++ imperial/imperial.c
@@ -1,19 +1,92 @@
+%{
+
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <string.h>
 #include <pcre.h>
 
-pcre* [];
+enum {SPACE = 128, DECIMAL, DOUBLE, WORD, VAR, IF};
 
-void initRegex() {
-	pcre_compile(const char *pattern, int options, const char **errptr, int *erroffset, const unsigned char *tableptr);
+%}
+
+I		[0-9]+
+D		{I}?"."{I}
+E		({I}|{D})e("+"|"-"){I}
+WORD	[a-z_][a-z_0-9]+
+A		[a-z]
+OP		":"|"<"|">"|"="
+
+%%
+\n|\r		return '\n';
+[:space:]	return SPACE;
+{I}			return DECIMAL;
+{E}			return DOUBLE;
+{OP}		return *yytext;
+if			return IF;
+{WORD}		return WORD;
+{A}			return VAR;
+%%
+
+char* PATH_TO_ROOT = ".";
+char* PATH_TO_CACHE = ".cache";
+char* OUT_FILE = NULL;
+
+
+void cc_load_cache(char* file) {
+	yyin = fopen(file, "rb");
+	if(!yyin) {
+		fprintf(stderr, "Файл `%s` не открыт: %s\n", file, strerror(errno));
+		exit(errno);
+	}
 }
 
+int cc_load_from_cache(char* file) {
+	char* ext = strrchr(file, '.');
+
+	if(ext) *ext = '\0';
+	PATH_TO_ROOT
+	 = malloc();
+	
+	struct stat sta;
+	if( stat(file, &sta) == 0 ) {
+		if(sta.st_mtime > )
+	}
+}
+
+void actionRun(char* file) {
+	// определяем расширение
+	char* ext = strrchr(file, '.');
+	if (ext) {	// есть расширение
+		if(strcmp(ext+1, "ic")==0) { cc_load_cache(file); cc_run(); return; }	// да это же кеш	
+	}
+
+	// пробуем извлечь из кеша
+	if(cc_load_from_cache(file)) {
+		cc_run();
+		return;
+	}
+	
+	yyin = fopen(file, "rb");
+	if(!yyin) {
+		fprintf(stderr, "Файл `%s` не открыт: %s\n", file, strerror(errno));
+		exit(errno);
+	}
+	
+	printf("compile %s\n", file);
+	
+	printf("compiled in %g sec\n", file);
+	
+	fclose(yyin);
+}
+
+int yywrap() { return 1; }
 
 void actionHelp() {
 	printf("This is Imperial programming language\n\n"
-	"usage: \n"
+	"\n"
 	);
 }
 
@@ -24,21 +97,27 @@ void actionVersion() {
 //extern char *optarg;
 //extern int optind, opterr, optopt;
 
-static const char *optString = "vh";
+static const char *optString = "ovh";
 
+// optional_argument
 static const struct option longOpts[] = {
-    { "version", no_argument, NULL, 'v' },
-    { "help", no_argument, NULL, 'h' },
-    { NULL, no_argument, NULL, 0 }
+	{ "out-file", required_argument, NULL, 'o' },
+	{ "version", no_argument, NULL, 'v' },
+	{ "help", no_argument, NULL, 'h' },
+	{ NULL, no_argument, NULL, 0 }
 };
 
 
 int main(int ac, char** av) {
 	int opt, longIndex = 0;
+	opterr = 0;	// не выводить ошибку
 	while((opt = getopt_long( ac, av, optString, longOpts, &longIndex )) != -1) switch(opt) {
 		case '?':
-			printf("exit\n");
+			printf("Не распознанная опция `%c`\n", optopt);
 			return 0;
+		case 'o':
+			OUT_FILE = optarg;
+			break;
 		case 'h':
 			actionHelp();
 			return 0;
@@ -48,11 +127,13 @@ int main(int ac, char** av) {
 	}
 	
 	if (optind < ac) {
-        while (optind < ac)
-            printf("%s ", av[optind++]);
-        printf("\n");
-    }
-	
+		while (optind < ac)	actionRun(av[optind++]);
+		printf("\n");
+	} else {
+		fprintf(stderr, "usage: %s [options] <file> [<file>]...\n", av[0]);
+		return 1;
+	}
+
 	return 0;
 }
 
---
+++ imperial/stringlists/global.c
@@ -0,0 +1,171 @@
+/***********************************************************************\
+*
+* $Revision: 986 $
+* $Date: 2012-07-07 08:46:35 +0200 (Sat, 07 Jul 2012) $
+* $Author: torsten $
+* Contents: global definitions
+* Systems: Linux
+*
+\***********************************************************************/
+
+/****************************** Includes *******************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#ifdef HAVE_BACKTRACE
+  #include <execinfo.h>
+#endif
+#include <assert.h>
+
+#include "global.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/**************************** Datatypes ********************************/
+
+/**************************** Variables ********************************/
+
+/****************************** Macros *********************************/
+
+/**************************** Functions ********************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __halt(const char   *filename,
+            unsigned int lineNb,
+            int          exitcode,
+            const char   *format,
+            ...
+           )
+{
+  va_list arguments;
+
+  assert(filename != NULL);
+  assert(format != NULL);
+
+  va_start(arguments,format);
+  vfprintf(stderr,format,arguments);
+  va_end(arguments);
+  fprintf(stderr," - halt in file %s, line %d\n", filename, lineNb);
+  exit(exitcode);
+}
+
+void __abort(const char   *filename,
+             unsigned int lineNb,
+             const char   *prefix,
+             const char   *format,
+             ...
+            )
+{
+  va_list arguments;
+
+  assert(filename != NULL);
+  assert(format != NULL);
+
+  if (prefix != NULL) fprintf(stderr,"%s", prefix);
+  va_start(arguments,format);
+  vfprintf(stderr,format,arguments);
+  va_end(arguments);
+  fprintf(stderr," - program aborted in file %s, line %d\n", filename, lineNb);
+  abort();
+}
+
+#if !defined(NDEBUG) && defined(HAVE_BACKTRACE)
+void debugDumpStackTrace(FILE *handle, const char *title, uint indent, void const *stackTrace[], uint stackTraceSize)
+{
+  const char **functionNames;
+  uint       i,z;
+
+  assert(handle != NULL);
+  assert(title != NULL);
+  assert(stackTrace != NULL);
+
+  for (i = 0; i < indent; i++) fprintf(handle," ");
+  fprintf(handle,"C stack trace: %s\n",title);
+  functionNames = (const char **)backtrace_symbols(stackTrace,stackTraceSize);
+  if (functionNames != NULL)
+  {
+    for (z = 1; z < stackTraceSize; z++)
+    {
+      for (i = 0; i < indent; i++) fprintf(handle," ");
+      fprintf(handle,"  %2d %p: %s\n",z,stackTrace[z],functionNames[z]);
+    }
+    free(functionNames);
+  }
+}
+
+void debugDumpCurrentStackTrace(FILE *handle, const char *title, uint indent)
+{
+  const int MAX_STACK_TRACE_SIZE = 256;
+
+  void *currentStackTrace;
+  int  currentStackTraceSize;
+
+  assert(handle != NULL);
+  assert(title != NULL);
+
+  currentStackTrace = malloc(sizeof(void*)*MAX_STACK_TRACE_SIZE);
+  if (currentStackTrace == NULL) return;
+
+  currentStackTraceSize = backtrace(currentStackTrace,MAX_STACK_TRACE_SIZE);
+  debugDumpStackTrace(handle,title,indent,currentStackTrace,currentStackTraceSize);
+
+  free(currentStackTrace);
+}
+#endif /* !defined(NDEBUG) && defined(HAVE_BACKTRACE) */
+
+#ifndef NDEBUG
+void debugDumpMemory(bool printAddress, const void *address, uint length)
+{
+  const byte *p;
+  uint       z,i;
+
+  z = 0;
+  while (z < length)
+  {
+    p = (const byte*)address+z;
+    if (printAddress) fprintf(stderr,"%08lx:",(unsigned long)p);
+    fprintf(stderr,"%08lx  ",(unsigned long)(p-(byte*)address));
+
+
+    for (i = 0; i < 16; i++)
+    {
+      if ((z+i) < length)
+      {
+        p = (const byte*)address+z+i;
+        fprintf(stderr,"%02x ",((uint)(*p)) & 0xFF);
+      }
+      else
+      {
+        fprintf(stderr,"   ");
+      }
+    }
+    fprintf(stderr,"  ");
+
+    for (i = 0; i < 16; i++)
+    {
+      if ((z+i) < length)
+      {
+        p = (const byte*)address+z+i;
+        fprintf(stderr,"%c",isprint((int)(*p))?(*p):'.');
+      }
+      else
+      {
+      }
+    }
+    fprintf(stderr,"\n");
+
+    z += 16;
+  }
+}
+#endif /* NDEBUG */
+
+#ifdef __cplusplus
+}
+#endif
+
+/* end of file */
